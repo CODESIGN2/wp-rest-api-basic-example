16605e0b003b715ccce99f4e4bec9b33755021b6
Lewis Cowles <lewiscowles@me.com>
2016-12-11 17:25:40 +0000

    Trivial Permission Example

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -32,6 +32,15 @@ class TestController extends WP_REST_Controller {
     }
 
     public function index_permissions_check(WP_REST_Request $request) {
-        return true;
+        // Really basic example for permissions checking
+        // (https://codex.wordpress.org/Roles_and_Capabilities#read)
+        return $this->user_permission_check($request->get_param('user_id'), 'read'); 
+    }
+
+    public function user_permission_check($user_id='', $cap='edit_posts') {
+        if(!empty($user_id)) {
+            return user_can($user_id, $cap);
+        }
+        return false;
     }
 }
