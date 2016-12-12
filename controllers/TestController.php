68fd154ffa6d3c1a3b09b4c3260866a973f3a379
Lewis Cowles <lewiscowles@me.com>
2016-12-12 03:53:43 +0000

    Re-factored Controller
     * Pulled in dependencies to replace hard-coded PoC functionality
     * Added `namespace` and `use` functionality
     * Manual Testing (should be automated, but we needed DI to be able to have those nice-things)

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,26 +1,48 @@
 <?php
 
+namespace lewiscowles;
+
 use WP_REST_Server;
 use WP_REST_Controller;
 use WP_REST_Request;
 use WP_REST_Response;
 
-class TestController extends WP_REST_Controller {
-    const RATE_FIELD = 'cd2_rest_rate';
-    const RATE_TIME = 60; // One Hour
+use lewiscowles\CreditDataInterface;
+use lewiscowles\CreditHandlerInterface;
+use lewiscowles\auth\SystemUserInterface;
+use lewiscowles\wordpress\auth\WPUser;
+use lewiscowles\storage\TTLStorageInterface;
+use lewiscowles\HTTP\REST\RestEndpointCreditData;
+use lewiscowles\HTTP\REST\RestEndpointTimedRateLimit;
+use lewiscowles\wordpress\storage\WPTransientStorage;
+
+class TestController extends \WP_REST_Controller {
+    const RATE_FIELD = 'cd2_rest_rate_user';
+    const RATE_TIME = 60; // One Minute
     const RATE_LIMIT = 5;
+
+    protected $limiter;
+    protected $auth;
 
     public function __construct() {
         $this->namespace = 'test/v1';
+        $this->auth = new WPUser();
+        $this->limiter = new RestEndpointTimedRateLimit(
+            self::RATE_FIELD."_".$this->auth->getPk(),
+            time(),
+            self::RATE_LIMIT,
+            new WPTransientStorage(),
+            self::RATE_TIME
+        );
     }
 
     public function register_routes() {
-        register_rest_route(
+        \register_rest_route(
             "{$this->namespace}",
             "/date",
             [
                 [
-                    'methods' => WP_REST_Server::READABLE,
+                    'methods' => \WP_REST_Server::READABLE,
                     'callback' => [$this, 'index'],
                     'permission_callback' => [$this, 'index_permissions_check'],
                     'args' => [],
@@ -29,48 +51,32 @@ class TestController extends WP_REST_Controller {
         );
     }
 
-    public function index(WP_REST_Request $request) {
-        $calls = $this->user_rate_check();
-        if(!($calls > 0)) {
-            return new WP_REST_Response("API Limit reached {$calls} left", 429);
+    public function index(\WP_REST_Request $request) {
+        $creditData = $this->limiter->getCreditData();
+        $refresh = $this->limiter->formatCreditReload($creditData);
+        if(!$this->limiter->checkCreditData($creditData)) {
+            $left = $creditData->getBalance();
+            $limit = self::RATE_LIMIT;
+            return new \WP_REST_Response(
+                "API Limit reached {$left}/{$limit} calls refresh in {$refresh}",
+                429
+            );
         }
-        $data = ['now'=> date('Y-m-d H:i:s')];
-        return new WP_REST_Response( $data, 200);
+
+        $data = [
+            'now'=> date('Y-m-d H:i:s'),
+            'time_to_refresh' => $refresh
+        ];
+
+        $creditData->deductBalance(1);
+        $this->limiter->setCreditData($creditData);
+
+        return new \WP_REST_Response( $data, 200);
     }
 
     public function index_permissions_check(WP_REST_Request $request) {
         // Really basic example for permissions checking
         // (https://codex.wordpress.org/Roles_and_Capabilities#read)
-        return $this->user_permission_check('read');
-    }
-
-    public function user_permission_check($cap='admin') {
-        return current_user_can($cap);
-    }
-
-    public function user_rate_check() {
-        if($this->user_permission_check('read')) {
-            $user_id = get_current_user_id();
-            $transient_name = self::RATE_FIELD."_{$user_id}";
-            $left = get_transient($transient_name);
-            if(!is_array($left)) {
-                set_transient($transient_name, [
-                    'limit'=>self::RATE_LIMIT,
-                    'time'=>time()
-                ], self::RATE_TIME);
-                $left = [
-                    'limit'=>self::RATE_LIMIT,
-                    'time'=>time()
-                ];
-            }
-            $ret = intval($left['limit']);
-            if($ret > 0) {
-                $left['limit']--;
-            }
-            $time = max(0, (self::RATE_TIME-(time()-$left['time'])));
-            set_transient($transient_name, $left, $time);
-            return $ret;
-        }
-        return false;
+        return $this->auth->hasCapability('read');
     }
 }
